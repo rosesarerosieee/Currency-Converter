23e97d1bb7382521b79482701c6b3c2107eb9493
rosesarerosieee <perezrose0211@gmail.com>
2024-06-07 13:39:02 +0800

    First commit in this repostiry

--- src/api/currencyapi.js
+++ src/api/currencyapi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "cur_live_OOS3ni5mmNI5oO2Z8T5Ny44w4kDpni4smxqSCq5C";
+const API_KEY = API_KEY
 const apiUrl = `https://api.currencyapi.com/v3/latest?apikey=${API_KEY}`;
 
 export const getExchangeRates = async () => {
