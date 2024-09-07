fab452365cb6df7deb4ac86a3b6f8d6a85bb3c24
rosesarerosieee <perezrose0211@gmail.com>
2024-09-08 01:11:13 +0800

    Fixed the issue in this project, for deployment

--- src/api/currencyapi.js
+++ src/api/currencyapi.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
-
 const API_KEY = process.env.REACT_APP_API_KEY;
 const apiUrl = `https://api.currencyapi.com/v3/latest?apikey=${API_KEY}`;
 
-export const getExchangeRates = async () => {
 
+console.log(process.env)
+
+
+export const getExchangeRates = async () => {
+    
     try{
         const response = await axios.get(apiUrl);
         return response.data.data;
