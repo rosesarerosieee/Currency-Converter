46cdabf0893c513be957d518224a1836a52c665e
rosesarerosieee <perezrose0211@gmail.com>
2024-10-17 08:34:44 +0800

    Fixing the .env problem

--- src/api/currencyapi.js
+++ src/api/currencyapi.js
@@ -1,23 +1,15 @@
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-const apiUrl = `https://api.currencyapi.com/v3/latest?apikey=${API_KEY}`;
+const apiUrl = `https://api.currencyapi.com/v3/latest?apikey=cur_live_OOS3ni5mmNI5oO2Z8T5Ny44w4kDpni4smxqSCq5C`;
 
-
-console.log(process.env)
-
+console.log(process.env);
 
 export const getExchangeRates = async () => {
-    
-    try{
-        const response = await axios.get(apiUrl);
-        return response.data.data;
-    }
-    catch(error){
-        console.error("Failed to catch currency!",error);
-        throw error;
-    }
-
+  try {
+    const response = await axios.get(apiUrl);
+    return response.data.data;
+  } catch (error) {
+    console.error("Failed to catch currency!", error);
+    throw error;
+  }
 };
-
-
